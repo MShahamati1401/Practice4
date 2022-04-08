0b2b6669a6b1f3377473b7e89f8350a4f883d443
MShahamati1401 <102240040+MShahamati1401@users.noreply.github.com>
2022-04-08 11:36:22 +0430

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -6,12 +6,15 @@ get_number = int(input("Please Inter Number: "))
 time_start = time.time()
 h=1
 flag = False
-get_number_temp = math.floor(get_number ** 0.5)
-for i in range(1, get_number_temp):
+# get_number_temp = math.floor(get_number ** 0.5)
+for i in range(1, get_number):
     h *= i
     if h == get_number:
         print("YES")
         flag = True
+        break
+    if h > get_number:
+        print("NO")
         break
 if flag == False:
     print("NO")
